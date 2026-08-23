8686094756e862718f42b2f15486a641974acf58
dotmantissa <negativemantissa@gmail.com>
2026-08-23 08:07:10 +0100

    Configure Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   nitro: {
-    preset: "node-server",
+    preset: "vercel",
   },
   tanstackStart: {
     client: { entry: "client" },
